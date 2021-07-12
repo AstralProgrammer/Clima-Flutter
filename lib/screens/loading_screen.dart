83bb650b1f6c682d87118a1b8bf4c3195428328f
Angel <darkangel0938@gmail.com>
2021-07-12 20:22:50 +0800

    Add weather temperature from loading_screen to the location_screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,8 @@
-
+import 'package:clima/screens/location_screen.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const kApiKey = '5e993c4377d33bc36dc45ceb1ad6ff4e';
 
@@ -24,17 +25,30 @@ class _LoadingScreenState extends State<LoadingScreen> {
     // get user coordinates
     Location location = Location();
     await location.getCurrentLocation();
-    latitude = location.latitude;
-    longitude = location.longitude;
 
     // Get weather data
-    NetworkHelper network = NetworkHelper('http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&APPID=$kApiKey');
+    NetworkHelper network = NetworkHelper(
+        'http://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&APPID=$kApiKey&units=metric');
 
-    var json = await network.getData();
+    var weatherData = await network.getData();
+
+    // show the weather page
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitFadingCube(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
